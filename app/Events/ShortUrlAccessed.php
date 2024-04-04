8304aec328d5ce7aab382004e76df09889f094ef
michael <michaelcosj@proton.me>
2024-04-04 15:45:41 +0100

    add event file

--- app/Events/ShortUrlAccessed.php
+++ app/Events/ShortUrlAccessed.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\ShortUrl;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -17,7 +18,7 @@ class ShortUrlAccessed
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct(public ShortUrl $shortUrl)
     {
         //
     }
